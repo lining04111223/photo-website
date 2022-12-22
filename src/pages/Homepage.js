d99351a01709e0eead16a3dea62fc4382980d7a8
Ning Li <lining04111223@gmail.com>
2022-12-22 12:07:43 -0800

    add status currentload

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,12 +5,15 @@ import Picture from "../components/Picture";
 const Homepage = () => {
   const [input, setInput] = useState("");
   let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "563492ad6f91700001000001c9d9683c94944ae9909af34e477795d4";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
 
   //fetch data
   const search = async (url) => {
+    setPage(2);
     const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
@@ -22,16 +25,43 @@ const Homepage = () => {
     setData(parseData.photos);
   };
 
+  const loadMore = async () => {
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+    }
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parseData = await dataFetch.json();
+    setData(data.concat(parseData.photos));
+  };
+
   //when page loads fetch one time
   useEffect(() => {
     search(initialURL);
   }, []);
 
+  useEffect(() => {
+    if (currentSearch === "") {
+      search(initialURL);
+    } else {
+      search(searchURL);
+    }
+  }, [currentSearch]);
+
   return (
     <div>
       <Search
         search={() => {
-          search(searchURL);
+          setCurrentSearch(input);
         }}
         setInput={setInput}
       />
@@ -40,6 +70,10 @@ const Homepage = () => {
           data.map((d) => {
             return <Picture data={d} />;
           })}
+      </div>
+
+      <div className="more">
+        <button onClick={loadMore}>Load more</button>
       </div>
     </div>
   );
